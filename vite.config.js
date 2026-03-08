7e1e0da57bad54696eca9037f4b88336aa9be504
Khaii-Htoo <khaihtooag@gmail.com>
2026-03-08 14:07:59 +0530

    feat: Add PWA icons and canvas dependency.

--- vite.config.js
+++ vite.config.js
@@ -19,17 +19,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-icon.png',
+            src: '/pwa-icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-icon.png',
+            src: '/pwa-icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-icon.png',
+            src: '/pwa-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
